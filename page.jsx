ea2b45ed56956adc8edd6506816503a793026ccc
Himanshu Sharma <him1972000@gmail.com>
2025-09-04 21:33:02 +0530

    Add files via upload

--- page.jsx
+++ page.jsx
@@ -1,61 +1,76 @@
 'use client'
-import { assets, blog_data } from '@/Assets/assets';
-import Footer from '@/Components/Footer';
-import axios from 'axios';
-import Image from 'next/image';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+import { assets } from '@/Assets/assets'
+import axios from 'axios'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
-const page = ({ params }) => {
+const page = () => {
 
-  const [data, setData] = useState(null);
-
-  const fetchBlogData = async () => {
-    const response = await axios.get('/api/blog', {
-      params: {
-        id: params.id
-      }
+    const [image,setImage] = useState(false);
+    const [data,setData] = useState({
+        title:"",
+        description:"",
+        category:"Startup",
+        author:"Alex Bennett",
+        authorImg:"/author_img.png"
     })
-    setData(response.data);
-  }
 
-  useEffect(() => {
-    fetchBlogData();
-  }, [])
+    const onChangeHandler = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data,[name]:value}));
+        console.log(data);
+    }
 
-  return (data ? <>
-    <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
-      <div className='flex justify-between items-center'>
-        <Link href='/'>
-          <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto' />
-        </Link>
-        <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
-          Get started <Image src={assets.arrow} alt='' />
-        </button>
-      </div>
-      <div className='text-center my-24'>
-        <h1 className='text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
-        <Image className='mx-auto mt-6 border border-white rounded-full' src={data.authorImg} width={60} height={60} alt='' />
-        <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto'>{data.author}</p>
-      </div>
-    </div>
-    <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
-      <Image className='border-4 border-white' src={data.image} width={800} height={480} alt='' />
-      
-      <div className='blog-content' dangerouslySetInnerHTML={{__html:data.description}}>
-        
-      </div>
-      <div className='my-24'>
-        <p className='text-black font font-semibold my-4'>Share this article on social media</p>
-        <div className='flex'>
-          <Image src={assets.facebook_icon} width={50} alt='' />
-          <Image src={assets.twitter_icon} width={50} alt='' />
-          <Image src={assets.googleplus_icon} width={50} alt='' />
-        </div>
-      </div>
-    </div>
-    <Footer />
-  </> : <></>
+    const onSubmitHandler = async (e) =>{
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('title',data.title);
+        formData.append('description',data.description);
+        formData.append('category',data.category);
+        formData.append('author',data.author);
+        formData.append('authorImg',data.authorImg);
+        formData.append('image',image);
+        const response = await axios.post('/api/blog',formData);
+        if (response.data.success) {
+            toast.success(response.data.msg);
+            setImage(false);
+            setData({
+              title:"",
+              description:"",
+              category:"Startup",
+              author:"Alex Bennett",
+              authorImg:"/author_img.png"
+            });
+        }
+        else{
+            toast.error("Error");
+        }
+    }
+
+  return (
+    <>
+      <form onSubmit={onSubmitHandler} className='pt-5 px-5 sm:pt-12 sm:pl-16'>
+        <p className='text-xl'>Upload thumbnail</p>
+        <label htmlFor="image">
+            <Image className='mt-4' src={!image?assets.upload_area:URL.createObjectURL(image)} width={140} height={70} alt=''/>
+        </label>
+        <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden required />
+        <p className='text-xl mt-4'>Blog title</p>
+        <input name='title' onChange={onChangeHandler} value={data.title} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='Type here' required />
+        <p className='text-xl mt-4'>Blog Description</p>
+        <textarea name='description' onChange={onChangeHandler} value={data.description} className='w-full sm:w-[500px] mt-4 px-4 py-3 border' type="text" placeholder='write content here' rows={6} required />
+        <p className='text-xl mt-4'>Blog category</p>
+        <select name="category" onChange={onChangeHandler} value={data.category} className='w-40 mt-4 px-4 py-3 border text-gray-500'>
+            <option value="Startup">Startup</option>
+            <option value="Technology">Technology</option>
+            <option value="Lifestyle">Lifestyle</option>
+        </select>
+        <br />
+        <button type="submit" className='mt-8 w-40 h-12 bg-black text-white'>ADD</button>
+      </form>
+    </>
   )
 }
 
