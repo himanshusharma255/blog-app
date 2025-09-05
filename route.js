cd848266507911804b033efd3277493eda325276
Himanshu Sharma <him1972000@gmail.com>
2025-09-05 19:51:27 +0530

    Add files via upload

--- route.js
+++ route.js
@@ -1,65 +1,28 @@
-import { ConnectDB } from "@/lib/config/db"
-import BlogModel from "@/lib/models/BlogModel";
-const { NextResponse } = require("next/server")
-import { writeFile } from 'fs/promises'
-const fs = require('fs')
+import { ConnectDB } from "@/lib/config/db";
+import EmailModel from "@/lib/models/EmailModel";
+import { NextResponse } from "next/server";
 
-const LoadDB = async () => {
-  await ConnectDB();
+const LoadDB = async () =>{
+    await ConnectDB();
 }
-
 LoadDB();
 
-
-// API Endpoint to get all blogs
-export async function GET(request) {
-
-  const blogId = request.nextUrl.searchParams.get("id");
-  if (blogId) {
-    const blog = await BlogModel.findById(blogId);
-    return NextResponse.json(blog);
-  }
-  else {
-    const blogs = await BlogModel.find({});
-    return NextResponse.json({ blogs })
-  }
+export async function POST(request){
+    const formData = await request.formData();
+    const emailData = {
+        email:`${formData.get('email')}`,
+    }
+    await EmailModel.create(emailData);
+    return NextResponse.json({success:true,msg:"Email Subscribed"})
 }
 
-
-// API Endpoint For Uploading Blogs
-export async function POST(request) {
-
-  const formData = await request.formData();
-  const timestamp = Date.now();
-
-  const image = formData.get('image');
-  const imageByteData = await image.arrayBuffer();
-  const buffer = Buffer.from(imageByteData);
-  const path = `./public/${timestamp}_${image.name}`;
-  await writeFile(path, buffer);
-  const imgUrl = `/${timestamp}_${image.name}`;
-
-  const blogData = {
-    title: `${formData.get('title')}`,
-    description: `${formData.get('description')}`,
-    category: `${formData.get('category')}`,
-    author: `${formData.get('author')}`,
-    image: `${imgUrl}`,
-    authorImg: `${formData.get('authorImg')}`
-  }
-
-  await BlogModel.create(blogData);
-  console.log("Blog Saved");
-
-  return NextResponse.json({ success: true, msg: "Blog Added" })
+export async function GET(request){
+    const emails = await EmailModel.find({});
+    return NextResponse.json({emails});
 }
 
-// Creating API Endpoint to delete Blog
-
-export async function DELETE(request) {
-  const id = await request.nextUrl.searchParams.get('id');
-  const blog = await BlogModel.findById(id);
-  fs.unlink(`./public${blog.image}`, () => { });
-  await BlogModel.findByIdAndDelete(id);
-  return NextResponse.json({ msg: "Blog Deleted" });
+export async function DELETE(request){
+    const id =  await request.nextUrl.searchParams.get("id");
+    await EmailModel.findByIdAndDelete(id);
+    return NextResponse.json({success:true,msg:"Email Deleted"})
 }
